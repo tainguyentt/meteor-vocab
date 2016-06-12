973e4fe3401dcfc8fc04c0928839525bbf193521
Le DacSy <Le DacSy>
2016-06-12 10:01:22 +0700

    added vote answer

--- client/templates/answer/answer-item/answer-item.js
+++ client/templates/answer/answer-item/answer-item.js
@@ -1,5 +1,36 @@
+Template.answerItem.onRendered(function() {
+	var userId = Meteor.userId();
+	if (this.data.voters.includes(userId)) {
+		Session.set('already-answer', true);
+	}
+});
+
 Template.answerItem.helpers({
-  createdTime: function() {
-    return this.createdAt.toDateString();
-  }
+	createdTime: function() {
+		return this.createdAt.toDateString();
+	},
+	voteCount: function() {
+		return this.voters.length;
+	},
+	isVoted: function() {
+        return Session.get('already-answer');
+    }
+});
+
+Template.answerItem.events({
+    'click #vote-answer': function(e) {
+        e.preventDefault();
+        var userId = Meteor.userId();
+        if (this.voters && this.voters.includes(userId)) {
+        	Session.set('already-answer', true);
+            alert('You cannot vote 2 times.');
+        } else {
+            var answerId = this._id;
+            Answers.update(answerId, { $push: { voters: userId } }, function(error) {
+                if (error) {
+                    console.log("Could not update question:", error);
+                }
+            })
+        }
+    }
 });
