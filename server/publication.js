06b7d4956f6efe9a69d1ad92812416ca3cfa5b3b
Tai Nguyen <tai.tan849@gmail.com>
2016-07-21 18:17:03 +0700

    fixed bug duplication in questions answered by me

--- server/publication.js
+++ server/publication.js
@@ -1,24 +1,28 @@
 // Client subscribes to this channel will get a list of all questions
 Meteor.publish('questions', function() {
-  return Questions.find();
+    return Questions.find();
 });
 
 // Client subscribes to this channel will get only one question according to the id param
 Meteor.publish('question', function(id) {
-  return Questions.find(id);
+    return Questions.find(id);
 });
 
 // Client subscribes to this channel will get a list of all answers
 Meteor.publish('answers', function(questionId) {
-  return Answers.find({questionId: questionId});
+    return Answers.find({ questionId: questionId });
 });
 
 // Client subscribes to this channel will get a list of all of my answers
 Meteor.publish('listQuestionAnswerByMe', function(userId) {
-	return Answers.find({createdBy: userId});
+    var userAnswers = Answers.find({ createdBy: userId });
+    var questiondIds = userAnswers.map(function(answer) {
+        return answer.questionId });
+    var questions = Questions.find({_id: {$in: questiondIds}});
+    return questions;
 });
 
 // Client subscribes to this channel will get a list of all of my question
 Meteor.publish('listMyQuestion', function(userId) {
-	return Questions.find({userId: userId});
+    return Questions.find({ userId: userId });
 })
