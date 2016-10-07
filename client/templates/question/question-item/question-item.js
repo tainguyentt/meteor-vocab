83c4bd1528fe164e6523ac49753b24408df8b2c9
Tai Nguyen <tai.tan849@gmail.com>
2016-10-08 04:12:04 +0700

    tried to fix bug vote

--- client/templates/question/question-item/question-item.js
+++ client/templates/question/question-item/question-item.js
@@ -17,4 +17,39 @@ Template.questionItem.helpers({
 	createdAt: function () {
 		return moment(this.created).locale('vi').fromNow();
 	},
+	voteCount: function () {
+		if (this.voters) {
+			return this.voters.length;
+		}
+	},
+	voted: function () {
+		return this.voters && this.voters.includes(Meteor.userId());
+	}
+});
+
+Template.questionItem.events({
+	'click .js-vote-question': function (e) {
+		e.preventDefault();
+		var userId = Meteor.userId();
+		var questionId = this._id;
+		var authorId = this.userId;
+		if (this.voters && this.voters.includes(userId)) {
+			Meteor.call('unvoteQuestion', questionId, userId, function (error) {
+				if (error) {
+					console.log('tai',error.reason);
+					throwError(error.reason);
+				} else {
+					updateUserPoints(authorId, -2);
+				}
+			})
+		} else {
+			Meteor.call('voteQuestion', questionId, userId, function (error) {
+				if (error) {
+					throwError(error.reason);
+				} else {
+					updateUserPoints(authorId, 2);
+				}
+			})
+		}
+	}
 });
